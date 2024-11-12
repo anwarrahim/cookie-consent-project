45d2464cc3f2389bf7998906bd9a65a9f6e5721a
Anwar Rahim <anwargithub@gmail.com>
2024-11-12 12:52:39 +0500

    new project

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 
 const modalCloseBtn = document.getElementById("modal-close-btn")
 const modal = document.getElementById("modal")
+const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById('modal-text')
+
 
 setTimeout(()=>{
     modal.style.display = 'inline'
-}, 3000)
+}, 1500)
+
 
 modalCloseBtn.addEventListener('click',  ()=>{
     modal.style.display= 'none'
 })
+
+
+consentForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    
+    modalText.innerHTML = `  <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
+    
+    setTimeout(()=>{
+        document.getElementById('upload-text').innerText=`Making the
+   sale...`
+    }, 1500)
+    
+})
+
+
+
 
 
 

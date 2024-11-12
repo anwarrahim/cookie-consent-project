96488b736eb797d8bf7c103bb48c67049a4965fb
Anwar Rahim <anwargithub@gmail.com>
2024-11-12 19:52:53 +0500

    new project

--- script.js
+++ script.js
@@ -19,9 +19,7 @@ consentForm.addEventListener("submit", (e)=>{
     e.preventDefault()
     
     const consentFormData = new FormData(consentForm)
-    
-    
-    
+    const name = consentFormData.get('visitorName')
     modalText.innerHTML = `  <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">
@@ -36,7 +34,7 @@ consentForm.addEventListener("submit", (e)=>{
     
     setTimeout(()=>{
         document.getElementById('modal-inner').innerHTML = `
-    <h2>Thanks you sucker! </h2>
+     <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
     <p>We just sold the rights to your eternal soul.</p>
     <div class="idiot-gif">
         <img src="images/pirate.gif">
